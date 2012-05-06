468ae578d736e95cb44f865e59e437f8fec3ded8
Elliot Winkler <elliot.winkler@gmail.com>
2012-05-06 03:18:56 -0600

    Split off date/time formats from Time extensions so they can be tested separately

---
+++ lib/tt/extensions/date_time_formats.rb
@@ -0,0 +1,25 @@
+
+Date::DATE_FORMATS[:relative_date] = lambda do |date|
+  case date - Date.today
+    when  0 then 'Today'
+    when -1 then 'Yesterday'
+    else         date.to_s(:mdy)
+  end
+end
+Date::DATE_FORMATS[:mdy] = lambda do |date|
+  date.strftime("#{date.month}/#{date.day}/%Y")
+end
+
+Time::DATE_FORMATS[:relative_date] = lambda do |time|
+  time.to_date.to_s(:relative_date)
+end
+Time::DATE_FORMATS[:mdy] = lambda do |time|
+  time.to_date.to_s(:mdy)
+end
+Time::DATE_FORMATS[:hms] = lambda do |time|
+  hour = time.hour
+  hour -= 12 if hour > 12
+  hour = 12  if hour == 0
+  ampm = time.hour >= 12 ? 'pm' : 'am'
+  time.strftime("#{hour}:%M#{ampm}")
+end
