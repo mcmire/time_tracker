82a39b2d3c00a4f8682fe679277d4cd118be4d51
Elliot Winkler <elliot.winkler@gmail.com>
2012-05-06 18:45:02 -0600

    Do not hardcode time zone; instead set to user's system time zone

--- lib/tt/core/time.rb
+++ lib/tt/core/time.rb
@@ -6,8 +6,7 @@ require 'active_support/core_ext/time/calculations'
 require 'active_support/core_ext/time/conversions'
 require 'active_support/core_ext/time/zones'
 
-# TODO: Make this configurable
-Time.zone = "Central Time (US & Canada)"
+Time.zone = Time.now.utc_offset
 
 require 'tt/extensions/ruby'
 require 'tt/extensions/date_time_formats'
