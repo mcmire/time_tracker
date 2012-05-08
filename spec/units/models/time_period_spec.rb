f9036ebc6d6786e0cd363b96b89407f0322db7f4
Elliot Winkler <elliot.winkler@gmail.com>
2012-05-07 19:57:52 -0600

    Fix tests for TimePeriod

--- spec/units/models/time_period_spec.rb
+++ spec/units/models/time_period_spec.rb
@@ -1,6 +1,11 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+require 'units/models/spec_helper'
+require 'tt/models/project'
+require 'tt/models/task'
+require 'tt/models/time_period'
 
 describe TimeTracker::Models::TimePeriod do
+  Models = TimeTracker::Models
 
   describe '.ended_today' do
     before do
@@ -9,14 +14,14 @@ describe TimeTracker::Models::TimePeriod do
       @end_of_today   = Time.zone.local(today.year, today.month, today.day, 23, 59, 59)
     end
     it "includes tasks ended today" do
-      start_of_today_task = Factory(:time_period, :ended_at => @start_of_today)
-      end_of_today_task = Factory(:time_period, :ended_at => @end_of_today)
-      TimeTracker::Models::TimePeriod.ended_today.to_a.must include(start_of_today_task)
-      TimeTracker::Models::TimePeriod.ended_today.to_a.must include(end_of_today_task)
+      start_of_today_task = FactoryGirl.create(:time_period, :ended_at => @start_of_today)
+      end_of_today_task = FactoryGirl.create(:time_period, :ended_at => @end_of_today)
+      described_class.ended_today.to_a.must include(start_of_today_task)
+      described_class.ended_today.to_a.must include(end_of_today_task)
     end
     it "excludes tasks ended in the past" do
-      yesterday_task = Factory(:time_period, :ended_at => @start_of_today-1)
-      TimeTracker::Models::TimePeriod.ended_today.to_a.must_not include(yesterday_task)
+      yesterday_task = FactoryGirl.create(:time_period, :ended_at => @start_of_today-1)
+      described_class.ended_today.to_a.must_not include(yesterday_task)
     end
   end
 
@@ -29,25 +34,25 @@ describe TimeTracker::Models::TimePeriod do
       @end_of_this_week   = Time.zone.local(saturday.year, saturday.month, saturday.day, 23, 59, 59)
     end
     it "includes tasks ended this week" do
-      start_of_this_week_task = Factory(:time_period, :ended_at => @start_of_this_week)
-      end_of_this_week_task = Factory(:time_period, :ended_at => @end_of_this_week)
-      TimeTracker::Models::TimePeriod.ended_this_week.to_a.must include(start_of_this_week_task)
-      TimeTracker::Models::TimePeriod.ended_this_week.to_a.must include(end_of_this_week_task)
+      start_of_this_week_task = FactoryGirl.create(:time_period, :ended_at => @start_of_this_week)
+      end_of_this_week_task = FactoryGirl.create(:time_period, :ended_at => @end_of_this_week)
+      described_class.ended_this_week.to_a.must include(start_of_this_week_task)
+      described_class.ended_this_week.to_a.must include(end_of_this_week_task)
     end
     it "excludes tasks ended in the past" do
-      yesterday_task = Factory(:time_period, :ended_at => @start_of_this_week-1)
-      TimeTracker::Models::TimePeriod.ended_this_week.to_a.must_not include(yesterday_task)
+      yesterday_task = FactoryGirl.create(:time_period, :ended_at => @start_of_this_week-1)
+      described_class.ended_this_week.to_a.must_not include(yesterday_task)
     end
   end
 
   describe '#info' do
     before do
-      @project = TimeTracker::Models::Project.new(:name => "some project")
-      @task = TimeTracker::Models::Task.new(:project => @project, :number => "1", :name => "some task")
+      @project = Models::Project.new(:name => "some project")
+      @task = Models::Task.new(:project => @project, :number => "1", :name => "some task")
     end
     context "if :include_day not given" do
       it "excludes the day portion" do
-        time_period = TimeTracker::Models::TimePeriod.new(
+        time_period = described_class.new(
           :task => @task,
           :started_at => Time.zone.local(2010, 1, 1, 3, 33),
           :ended_at => Time.zone.local(2010, 1, 1, 11, 11)
@@ -60,7 +65,7 @@ describe TimeTracker::Models::TimePeriod do
         ]
       end
       it "creates fake time periods if starts_at is not the same day as ends_at" do
-        time_period = TimeTracker::Models::TimePeriod.new(
+        time_period = described_class.new(
           :task => @task,
           :started_at => Time.zone.local(2010, 1, 1, 3, 33),
           :ended_at => Time.zone.local(2010, 1, 3, 11, 11)
@@ -81,7 +86,7 @@ describe TimeTracker::Models::TimePeriod do
         ]
       end
       it "returns the arrays in reverse order if :reverse given" do
-        time_period = TimeTracker::Models::TimePeriod.new(
+        time_period = described_class.new(
           :task => @task,
           :started_at => Time.zone.local(2010, 1, 1, 3, 33),
           :ended_at => Time.zone.local(2010, 1, 3, 11, 11)
@@ -102,7 +107,7 @@ describe TimeTracker::Models::TimePeriod do
         ]
       end
       it "removes fake time periods that don't satisfy the :where_date option" do
-        time_period = TimeTracker::Models::TimePeriod.new(
+        time_period = described_class.new(
           :task => @task,
           :started_at => Time.zone.local(2010, 1, 1, 3, 33),
           :ended_at => Time.zone.local(2010, 1, 3, 11, 11)
@@ -119,7 +124,7 @@ describe TimeTracker::Models::TimePeriod do
     end
     context "if :include_day given" do
       it "includes the day part" do
-        time_period = TimeTracker::Models::TimePeriod.new(
+        time_period = described_class.new(
           :task => @task,
           :started_at => Time.zone.local(2010, 1, 1, 3, 33),
           :ended_at => Time.zone.local(2010, 1, 3, 11, 11)
@@ -128,7 +133,7 @@ describe TimeTracker::Models::TimePeriod do
           ['1/1/2010', ', ', '3:33am', ' - ', '1/3/2010', ', ', '11:11am', ' ', '[', '#1', ']', " ", 'some project / some task']
       end
       it "includes the day part even if days are the same" do
-        time_period = TimeTracker::Models::TimePeriod.new(
+        time_period = described_class.new(
           :task => @task,
           :started_at => Time.zone.local(2010, 1, 1, 3, 33),
           :ended_at => Time.zone.local(2010, 1, 1, 11, 11)
@@ -142,7 +147,7 @@ describe TimeTracker::Models::TimePeriod do
 
   describe '#duration' do
     it "returns the seconds between started_at and ended_at" do
-      period = TimeTracker::Models::TimePeriod.new(
+      period = described_class.new(
         :started_at => Time.zone.local(2010, 1, 1, 0, 0, 0),
         :ended_at => Time.zone.local(2010, 1, 1, 2, 30, 0)
       )
